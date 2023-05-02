03dc4820fa204ba2c188bc2e3209675d72c19400
ariaan ahmed <you@example.com>
2023-05-02 12:31:09 +0600

    server is running successfully

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ const port = process.env.PORT || 5000;
 
 const cors = require('cors')
 
+const chefs = require('./data/chefs.json');
+
 app.get('/', (req, res) => {
-    res.send('server is running')
+    res.send('chefs server is running')
 })
 
 app.use(cors());
+
+app.get('/chefs', (req, res) => {
+    res.send(chefs)
+})
+
 
 app.listen(port, () => {
     console.log(`server is live at port: ${port}`)
